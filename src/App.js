4e3f98d5e2e4c17c0cabc8fd8abbb845e7121d70
acworkma <adworkma@microsoft.com>
2024-10-08 16:47:16 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import ImageUpload from './ImageUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Image Upload</h1>
+      <ImageUpload />
+    </div>
+  );
+}
+
+export default App;
